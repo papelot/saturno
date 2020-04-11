1ca9b5e46f6e4f136ba8b6a7a0d82e0159f2208f
Ubuntu <ubuntu@ip-172-31-21-32.us-east-2.compute.internal>
2020-04-11 09:17:43 +0000

    working on sign-up-2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-       flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,10 +1,18 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
-   test "login with invalid information" do
+  
+  def setup
+    @user = users(:michael)
+  end
+  
+  
+  
+   test "login with valid email/invalid password" do
     get login_path
     assert_template 'sessions/new'
-    post login_path, params: { session: { email: "", password: "" } }
+    post login_path, params: { session: { email:    "example@railstutorial.org",
+                                          password: "invalid" } }
     assert_template 'sessions/new'
     assert_not flash.empty?
     get root_path
