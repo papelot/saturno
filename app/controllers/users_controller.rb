43f54e0aeccfc90f6d1e6f585f482dca31ac67ab
Ubuntu <ubuntu@ip-172-31-39-147.us-east-2.compute.internal>
2020-04-19 15:00:50 +0000

    updating user -3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,12 @@
 class UsersController < ApplicationController
+  
+  before_action :logged_in_user, only: [:edit, :update, :index]
+  before_action :correct_user,   only: [:edit, :update]  
+  
+  def index
+    @users = User.paginate(page: params[:page], per_page: 30)
+  end 
+  
   def new
   end
   
@@ -22,7 +30,17 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
+  end
+  
+  def update
+    if @user.update(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
    private
@@ -31,4 +49,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    
+    def logged_in_user
+      unless logged_in?
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
+    
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
 end
+ 
