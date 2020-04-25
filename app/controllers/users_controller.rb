1b5de519b08d380c27e003ca621f89620880bc86
Ubuntu <ubuntu@ip-172-31-19-162.us-east-2.compute.internal>
2020-04-25 22:18:28 +0000

    Add account activation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,8 @@ class UsersController < ApplicationController
   before_action :admin_user,     only: :destroy
     
   def index
-    @users = User.paginate(page: params[:page], per_page: 30)
+    #@users = User.paginate(page: params[:page], per_page: 30)
+    @users = User.where(activated: true).paginate(page: params[:page])
   end 
   
   def new
@@ -13,6 +14,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
   
   def new
@@ -22,9 +24,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
-      log_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      @user.send_activation_email
+      flash[:info] = "Controlla la tua email per attivare il tuo account"
+      redirect_to root_url
     else
       render 'new'
     end
